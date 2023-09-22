bd946c6ed7f454e01523057be06cd5d411d8cf9d
StachuraMaciej <41348565+StachuraMaciej@users.noreply.github.com>
2023-09-22 16:48:10 +0200

    updated searchbar.ts

--- test/specs/e2e/searchbar.ts
+++ test/specs/e2e/searchbar.ts
@@ -13,38 +13,29 @@ describe("E2E search bar", async () => {
   });
 
   it("Should accept Rodo", async () => {
-    // Zaimportowanie metody z folderu pages
     await rodoConfirm.acceptRodo();
-    await browser.pause(3000);
   });
 
   it("Should click on the icon", async () => {
-    // Zaimportowanie metody z folderu pages
     await searchbarPage.clickIcon();
     await expect(browser).toHaveUrl(searchStep);
   });
 
   it("Should type search value and verify and verify visible list", async () => {
-    // Zaimportowanie metody z folderu pages
     await searchbarPage.typeText(textOne);
     await searchbarPage.visibleList();
-    await browser.pause(3000);
   });
 
   it("Should accept Rodo", async () => {
-    // Zaimportowanie metody z folderu pages
     await rodoConfirm.acceptRodo();
-    await browser.pause(3000);
   });
 
   it("Should click on see all books button", async () => {
-    // Zaimportowanie metody z folderu pages
     await searchbarPage.clickonbtnTwoAllBooks();
     await expect(browser).toHaveUrl(searchPageURL);
   });
 
   it("Should verify text of title and number of books", async () => {
-    // Zaimportowanie metody z folderu pages
     await SearchResultPage.getPageTitle();
     const numberOfBooks: number = await searchbarPage.getNumberOfBooks();
 
